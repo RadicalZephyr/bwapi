RadicalZephyr/bwapi

commit 2aa69c15585c4c806af7dd59883a5d325c041e03

fixed some bugs and optimized some of the unit set update code.
git-svn-id: https://bwapi.googlecode.com/svn/trunk@2567 273ecf2c-f24b-0410-b6a7-e9298bb4873f

bwapi/BWAPI/Source/BWAPI/GameImpl.h
@@ -257,30 +257,23 @@ namespace BWAPI
       BW::dialog *canvas;
       BW::dialog *myDlg;
       Server server;
+      std::vector<BWAPI::UnitImpl*> lastEvadedUnits;
     private :
       HMODULE hMod;
       void saveSelected();
       Map map;
 
-      std::set<BWAPI::UnitImpl*> newUnits; //units entering aliveUnits set on current frame
       std::set<BWAPI::UnitImpl*> aliveUnits; //units alive on current frame
       std::set<BWAPI::UnitImpl*> dyingUnits; //units leaving aliveUnits set on current frame
 
-      std::set<BWAPI::UnitImpl*> discoverUnits; //units entering accessibleUnits set on current frame
+      std::vector<BWAPI::UnitImpl*> discoverUnits; //units entering accessibleUnits set on current frame
       std::set<BWAPI::Unit*> accessibleUnits; //units that are accessible to the client on current frame
-      std::set<BWAPI::UnitImpl*> evadeUnits; //units leaving accessibleUnits set on current frame
+      std::vector<BWAPI::UnitImpl*> evadeUnits; //units leaving accessibleUnits set on current frame
       
-      std::set<BWAPI::UnitImpl*> showUnits; // units entering visibleUnits set on current frame
-      std::set<BWAPI::UnitImpl*> visibleUnits; //units that are visible to the client on current frame
-      std::set<BWAPI::UnitImpl*> hideUnits; //units leaving visibleUnits set on current frame
-
-      std::set<BWAPI::UnitImpl*> createUnits; // intersection of newUnits and accessibleUnits
-      std::set<BWAPI::Unit*> notDestroyedUnits;// all units that have been discovered at some point and have not been destroyed while accessible
-      std::set<BWAPI::UnitImpl*> destroyUnits; // intersection of dyingUnits and accessibleUnits
+      std::vector<BWAPI::UnitImpl*> visibleUnits; //units that are visible to the client on current frame
+      std::vector<BWAPI::UnitImpl*> hideUnits; //units leaving visibleUnits set on current frame
 
 
-      std::set<BWAPI::UnitImpl*> morphUnits;
-      std::set<BWAPI::UnitImpl*> renegadeUnits;
       std::set<BWAPI::Unit*> selectedUnitSet;
       std::set<BWAPI::Unit*> emptySet;
       std::set<TilePosition> startLocations;
@@ -293,7 +286,7 @@ namespace BWAPI
       std::set<BWAPI::Unit*> geysers;
       std::set<BWAPI::Unit*> neutralUnits;
       std::set<BWAPI::Bullet*> bullets;
-      std::list<BWAPI::UnitImpl*> pylons;
+      std::set<BWAPI::UnitImpl*> pylons;
       Util::RectangleArray<std::set<Unit*> > unitsOnTileData;
 
       std::set<BWAPI::Unit*> staticMinerals;

bwapi/BWAPI/Source/BWAPI/GameInternals.cpp
@@ -206,14 +206,18 @@ namespace BWAPI
         {
           if (this->BWAPIPlayer->isVictorious())
           {
-            this->client->onEnd(true);
             events.push_back(Event::MatchEnd(true));
+            events.push_back(Event::MatchFrame());
+            processEvents();
+            server.update();
             this->calledOnEnd = true;
           }
           if (this->BWAPIPlayer->isDefeated())
           {
-            this->client->onEnd(false);
             events.push_back(Event::MatchEnd(false));
+            events.push_back(Event::MatchFrame());
+            processEvents();
+            server.update();
             this->calledOnEnd = true;
           }
         }
@@ -228,8 +232,10 @@ namespace BWAPI
           }
           if (allDone)
           {
-            this->client->onEnd(false);
             events.push_back(Event::MatchEnd(false));
+            events.push_back(Event::MatchFrame());
+            processEvents();
+            server.update();
             this->calledOnEnd = true;
           }
         }
@@ -1100,25 +1106,23 @@ namespace BWAPI
               win = false;
           }
         }
-        this->client->onEnd(win);
         events.push_back(Event::MatchEnd(win));
+        events.push_back(Event::MatchFrame());
+        processEvents();
+        server.update();
         this->calledOnEnd = true;
       }
       delete this->client;
       this->client = NULL;
     }
-    newUnits.clear();
     aliveUnits.clear();
     dyingUnits.clear();
     discoverUnits.clear();
     accessibleUnits.clear();
     evadeUnits.clear();
-    showUnits.clear();
+    lastEvadedUnits.clear();
     visibleUnits.clear();
     hideUnits.clear();
-    createUnits.clear();
-    notDestroyedUnits.clear();
-    destroyUnits.clear();
     selectedUnitSet.clear();
     emptySet.clear();
     startLocations.clear();
@@ -1163,7 +1167,6 @@ namespace BWAPI
       if (unitArray[i] == NULL)
         continue;
       unitArray[i]->userSelected      = false;
-      unitArray[i]->isNew             = false;
       unitArray[i]->isAlive           = false;
       unitArray[i]->isDying           = false;
       unitArray[i]->wasAlive          = false;
@@ -1217,7 +1220,7 @@ namespace BWAPI
   {
     map.copyToSharedMemory();
   }
-  bool isAlive(UnitImpl* i, bool isHidden=false)
+  bool inline isAlive(UnitImpl* i, bool isHidden=false)
   {
     if (i->getOriginalRawData->orderID == BW::OrderID::Die) return false;
     UnitType _getType = BWAPI::UnitType(i->getOriginalRawData->unitID.id);
@@ -1244,8 +1247,8 @@ namespace BWAPI
       u->wasAlive = true;
       u->isAlive = false;
       u->isDying = true;
-      u->isNew = false;
     }
+    lastEvadedUnits = evadeUnits;
     for each(UnitImpl* u in accessibleUnits)
       u->wasAccessible = true;
     for each(UnitImpl* u in evadeUnits)
@@ -1257,7 +1260,6 @@ namespace BWAPI
 
     dyingUnits = aliveUnits;
     aliveUnits.clear();
-    newUnits.clear();
 
     //compute alive, new, and dying units
     for(UnitImpl* u = UnitImpl::BWUnitToBWAPIUnit(*BW::BWDATA_UnitNodeList_VisibleUnit_First); u!=NULL ; u = u->getNext())
@@ -1268,11 +1270,6 @@ namespace BWAPI
         u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
-        if (u->wasAlive==false)
-        {
-          u->isNew = true;
-          newUnits.insert(u);
-        }
         u->updateInternalData();
       }
     }
@@ -1284,11 +1281,6 @@ namespace BWAPI
         u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
-        if (u->wasAlive==false)
-        {
-          u->isNew = true;
-          newUnits.insert(u);
-        }
         u->updateInternalData();
       }
     }
@@ -1300,11 +1292,6 @@ namespace BWAPI
         u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
-        if (u->wasAlive==false)
-        {
-          u->isNew = true;
-          newUnits.insert(u);
-        }
         u->updateInternalData();
       }
     }
@@ -1319,50 +1306,45 @@ namespace BWAPI
     accessibleUnits.clear();
     evadeUnits.clear();
 
-    showUnits.clear();
     visibleUnits.clear();
     hideUnits.clear();
-
-    createUnits.clear();
-    destroyUnits.clear();
 
     for each(UnitImpl* u in aliveUnits)
     {
       if (u->canAccess())
       {
         if (u->wasAccessible==false)
-          discoverUnits.insert(u);
+          discoverUnits.push_back(u);
         if (u->wasAlive==false)
-          createUnits.insert(u);
+          events.push_back(Event::UnitCreate(u));
         accessibleUnits.insert(u);
-        notDestroyedUnits.insert(u);
       }
       else
       {
         if (u->wasAccessible)
-          evadeUnits.insert(u);
+          evadeUnits.push_back(u);
       }
       if (u->isVisible())
       {
         if (u->wasVisible==false)
-          showUnits.insert(u);
-        visibleUnits.insert(u);
+          events.push_back(Event::UnitShow(u));
+        visibleUnits.push_back(u);
       }
       else
       {
         if (u->wasVisible)
-          hideUnits.insert(u);
+          hideUnits.push_back(u);
       }
     }
     for each(UnitImpl* u in dyingUnits)
     {
       if (u->wasAccessible)
       {
-        evadeUnits.insert(u);
-        destroyUnits.insert(u);
+        evadeUnits.push_back(u);
+        events.push_back(Event::UnitDestroy(u));
       }
       if (u->wasVisible)
-        hideUnits.insert(u);
+        hideUnits.push_back(u);
     }
   }
   void GameImpl::extractUnitData()
@@ -1383,13 +1365,24 @@ namespace BWAPI
       i->startingAttack           = airWeaponCooldown > i->lastAirWeaponCooldown || groundWeaponCooldown > i->lastGroundWeaponCooldown;
       i->lastAirWeaponCooldown    = airWeaponCooldown;
       i->lastGroundWeaponCooldown = groundWeaponCooldown;
-    }
-
-    for each (UnitImpl* u in accessibleUnits)
-    {
-      if (u->getID()==-1)
-        u->setID(server.getUnitID(u));
-      u->updateData();
+      if (i->canAccess())
+      {
+        if (i->getID()==-1)
+          i->setID(server.getUnitID(i));
+        i->updateData();
+      }
+      if (i->getOriginalRawData->unitID == BW::UnitID::Terran_NuclearMissile)
+      {
+        if (i->nukeDetected==false)
+        {
+          i->nukeDetected=true;
+          Position target(i->getOriginalRawData->orderTargetPos.x,i->getOriginalRawData->orderTargetPos.y);
+          if (isFlagEnabled(Flag::CompleteMapInformation) || isVisible(target.x()/32,target.y()/32))
+            events.push_back(Event::NukeDetect(target));
+          else
+            events.push_back(Event::NukeDetect(Positions::Unknown));
+        }
+      }
     }
   }
   void GameImpl::augmentUnitData()
@@ -1452,21 +1445,53 @@ namespace BWAPI
   }
   void GameImpl::computeSecondaryUnitSets()
   {
-    for each (Player* i in playerSet)
-      ((PlayerImpl*)i)->units.clear();
-
     /* Clear all units on tile data */
     for (int y = 0; y < Map::getHeight(); y++)
       for (int x = 0; x < Map::getWidth(); x++)
         this->unitsOnTileData[x][y].clear();
 
-    neutralUnits.clear();
-    minerals.clear();
-    geysers.clear();
-    pylons.clear();
-    morphUnits.clear();
-    renegadeUnits.clear();
-
+    for each(UnitImpl* u in discoverUnits)
+    {
+      events.push_back(Event::UnitDiscover(u));
+      ((PlayerImpl*)u->getPlayer())->units.insert(u);
+      if (u->getPlayer()->isNeutral())
+      {
+        neutralUnits.insert(u);
+        if (u->getType() == UnitTypes::Resource_Mineral_Field)
+          minerals.insert(u);
+        else
+        {
+          if (u->getType() == UnitTypes::Resource_Vespene_Geyser)
+            geysers.insert(u);
+        }
+      }
+      else
+      {
+        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+          pylons.insert(u);
+      }
+    }
+    for each(UnitImpl* u in evadeUnits)
+    {
+      events.push_back(Event::UnitEvade(u));
+      ((PlayerImpl*)u->getPlayer())->units.erase(u);
+      if (u->getPlayer()->isNeutral())
+      {
+        neutralUnits.erase(u);
+        if (u->getType() == UnitTypes::Resource_Mineral_Field)
+          minerals.erase(u);
+        else
+        {
+          if (u->getType() == UnitTypes::Resource_Vespene_Geyser)
+            geysers.erase(u);
+        }
+      }
+      else
+      {
+        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+          pylons.erase(u);
+      }
+    }
     for each (UnitImpl* i in accessibleUnits)
     {
       int startX = (i->_getPosition.x() - i->_getType.dimensionLeft()) / BW::TILE_SIZE;
@@ -1476,41 +1501,17 @@ namespace BWAPI
       for (int x = startX; x < endX; x++)
         for (int y = startY; y < endY; y++)
           unitsOnTileData[x][y].insert(i);
-      ((PlayerImpl*)i->getPlayer())->units.insert(i);
-      if (i->_getPlayer->isNeutral())
-      {
-        neutralUnits.insert(i);
-        if (i->_getType == UnitTypes::Resource_Mineral_Field)
-          minerals.insert(i);
-        else
-        {
-          if (i->_getType == UnitTypes::Resource_Vespene_Geyser)
-            geysers.insert(i);
-        }
-      }
-      else
-      {
-        if (i->_getPlayer == (Player*)this->BWAPIPlayer && i->_getType == UnitTypes::Protoss_Pylon && i->_isCompleted)
-          pylons.push_back(i);
-      }
       if (i->lastType != i->_getType && i->lastType != UnitTypes::Unknown && i->_getType != UnitTypes::Unknown)
-        morphUnits.insert(i);
+        events.push_back(Event::UnitMorph(i));
       if (i->lastPlayer != i->_getPlayer && i->lastPlayer != NULL && i->_getPlayer != NULL)
-        renegadeUnits.insert(i);
+      {
+        events.push_back(Event::UnitRenegade(i));
+        ((PlayerImpl*)i->lastPlayer)->units.erase(i);
+        ((PlayerImpl*)i->_getPlayer)->units.insert(i);
+      }
       i->lastPlayer = i->_getPlayer;
       i->lastType = i->_getType;
     }
-  }
-  //---------------------------------------------- UPDATE UNITS ----------------------------------------------
-  void GameImpl::updateUnits()
-  {
-    computeUnitExistence();
-    computePrimaryUnitSets();
-    extractUnitData();
-    augmentUnitData();
-    applyLatencyCompensation();
-    computeSecondaryUnitSets();
-
     if (this->staticNeutralUnits.empty())
     {
       foreach (UnitImpl* i, aliveUnits)
@@ -1529,38 +1530,23 @@ namespace BWAPI
         }
       }
     }
-    //create events
-    for each(UnitImpl* u in aliveUnits)
-    {
-      if (u->getOriginalRawData->unitID == BW::UnitID::Terran_NuclearMissile)
-      {
-        if (u->nukeDetected==false)
-        {
-          u->nukeDetected=true;
-          Position target(u->getOriginalRawData->orderTargetPos.x,u->getOriginalRawData->orderTargetPos.y);
-          if (isFlagEnabled(Flag::CompleteMapInformation) || isVisible(target.x()/32,target.y()/32))
-            events.push_back(Event::NukeDetect(target));
-          else
-            events.push_back(Event::NukeDetect(Positions::Unknown));
-        }
-      }
-    }
-    for each(UnitImpl* u in createUnits)
-      events.push_back(Event::UnitCreate(u));
-    for each(UnitImpl* u in showUnits)
-      events.push_back(Event::UnitShow(u));
-    for each(UnitImpl* u in morphUnits)
-      events.push_back(Event::UnitMorph(u));
-    for each(UnitImpl* u in renegadeUnits)
-      events.push_back(Event::UnitRenegade(u));
+  }
+  //---------------------------------------------- UPDATE UNITS ----------------------------------------------
+  void GameImpl::updateUnits()
+  {
+    computeUnitExistence();
+    computePrimaryUnitSets();
+    extractUnitData();
+    augmentUnitData();
+    applyLatencyCompensation();
+    computeSecondaryUnitSets();
     for each(UnitImpl* u in hideUnits)
       events.push_back(Event::UnitHide(u));
-    for each(UnitImpl* u in destroyUnits)
-      events.push_back(Event::UnitDestroy(u));
   }
   void GameImpl::processEvents()
   {
     if (client==NULL) return;
+    if (server.isConnected()) return;
     for(std::list<Event>::iterator e=events.begin();e!=events.end();e++)
     {
       EventType::Enum et=e->type;
@@ -1588,6 +1574,12 @@ namespace BWAPI
         break;
         case EventType::NukeDetect:
           client->onNukeDetect(e->position);
+        break;
+        case EventType::UnitDiscover:
+          client->onUnitDiscover(e->unit);
+        break;
+        case EventType::UnitEvade:
+          client->onUnitEvade(e->unit);
         break;
         case EventType::UnitCreate:
           client->onUnitCreate(e->unit);

bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
@@ -26,7 +26,6 @@ namespace BWAPI
       : getOriginalRawData(originalUnit)
       , index(index)
       , userSelected(false)
-      , isNew(false)
       , isAlive(false)
       , isDying(false)
       , wasAlive(false)
@@ -638,6 +637,14 @@ namespace BWAPI
   //--------------------------------------------- IS VISIBLE -------------------------------------------------
   bool UnitImpl::isVisible() const
   {
+    if (Broodwar->self()==NULL)
+    {
+      for(int i=0;i<9;i++)
+      {
+        if (self->isVisible[i]) return true;
+      }
+      return false;
+    }
     return self->isVisible[Broodwar->self()->getID()];
   }
   //--------------------------------------------- IS VISIBLE -------------------------------------------------
@@ -2037,7 +2044,6 @@ namespace BWAPI
     getOriginalRawData = NULL;
     index              = 0xFFFF;
     userSelected       = false;
-    isNew              = false;
     isAlive            = false;
     isDying            = false;
     wasAlive           = false;

bwapi/BWAPI/Source/BWAPI/UnitImpl.h
@@ -262,7 +262,6 @@ namespace BWAPI
       Player* lastPlayer;
       
       int id;
-      bool isNew;
       bool isAlive;
       bool isDying;
       bool wasAlive;

commit 0898bc14f54082ff007d62f7469281a475d0de03

fixed some bugs I accidentally introduced in r2567.

bwapi/BWAPI/Source/BWAPI/GameImpl.cpp
@@ -555,6 +555,7 @@ namespace BWAPI
     /* Loop through all pylons for the current player */
     foreach (UnitImpl* i, pylons)
     {
+      if (i->isCompleted()==false) continue;
       int px = i->getTilePosition().x();
       int py = i->getTilePosition().y();
       int bx = x - px + 7;

bwapi/BWAPI/Source/BWAPI/GameImpl.h
@@ -211,7 +211,6 @@ namespace BWAPI
       void mouseDown(int x, int y);
       void mouseUp(int x, int y);
       void changeSlot(BW::Orders::ChangeSlot::Slot slot, u8 slotID);
-      void setOnStartCalled(bool onStartCalled);
       void IssueCommand(PBYTE pbBuffer, u32 iSize);
       void addToCommandBuffer(Command* command);
       void onGameStart();
@@ -226,7 +225,6 @@ namespace BWAPI
       bool inScreen(int ctype, int x1, int y1, int x2, int y2, int x3, int y3);
       void lockFlags();
       bool enabled;
-      bool isOnStartCalled() const;
       bool _isInGame() const;
       bool _isSinglePlayer() const;
       bool _isBattleNet();
@@ -258,6 +256,7 @@ namespace BWAPI
       BW::dialog *myDlg;
       Server server;
       std::vector<BWAPI::UnitImpl*> lastEvadedUnits;
+      bool onStartCalled;
     private :
       HMODULE hMod;
       void saveSelected();
@@ -295,7 +294,6 @@ namespace BWAPI
 
       /** Count of game-frames passed from game start. */
       int frameCount;
-      bool onStartCalled;
       UnitImpl* unitArray[BW::UNIT_ARRAY_MAX_LENGTH];
       BulletImpl* bulletArray[BW::BULLET_ARRAY_MAX_LENGTH];
       std::vector<std::vector<Command *> > commandBuffer;

bwapi/BWAPI/Source/BWAPI/GameInternals.cpp
@@ -195,7 +195,7 @@ namespace BWAPI
     try
     {
       this->inUpdate = true;
-      if (!this->isOnStartCalled())
+      if (!onStartCalled)
         this->onGameStart();
       
       if (!this->enabled)
@@ -702,16 +702,6 @@ namespace BWAPI
   {
     return (*BW::BWDATA_IsMultiplayer == 0);
   }
-  //------------------------------------------- IS ON START CALLED -------------------------------------------
-  bool GameImpl::isOnStartCalled() const
-  {
-    return this->onStartCalled;
-  }
-  //------------------------------------------ SET ON START CALLED -------------------------------------------
-  void GameImpl::setOnStartCalled(bool onStartCalled)
-  {
-    this->onStartCalled = onStartCalled;
-  }
   //------------------------------------------------ IS IN GAME ----------------------------------------------
   bool GameImpl::_isInGame() const
   {
@@ -873,13 +863,13 @@ namespace BWAPI
   void GameImpl::onGameStart()
   {
     /* initialize the variables */
-    this->frameCount  = 0;
-    this->textSize    = 1;
-    this->setOnStartCalled(true);
-    this->BWAPIPlayer = NULL;
-    this->opponent    = NULL;
-    this->calledOnEnd = false;
-    this->bulletCount = 0;
+    frameCount  = 0;
+    textSize    = 1;
+    onStartCalled = true;
+    BWAPIPlayer = NULL;
+    opponent    = NULL;
+    calledOnEnd = false;
+    bulletCount = 0;
 
     /* set all the flags to the default of disabled */
     for (int i = 0; i < FLAG_COUNT; i++)
@@ -1078,7 +1068,7 @@ namespace BWAPI
   //---------------------------------------------- ON GAME END -----------------------------------------------
   void GameImpl::onGameEnd()
   {
-    this->setOnStartCalled(false);
+    onStartCalled = false;
 
     if ( myDlg )
     {
@@ -1168,7 +1158,6 @@ namespace BWAPI
         continue;
       unitArray[i]->userSelected      = false;
       unitArray[i]->isAlive           = false;
-      unitArray[i]->isDying           = false;
       unitArray[i]->wasAlive          = false;
       unitArray[i]->wasAccessible     = false;
       unitArray[i]->wasVisible        = false;
@@ -1246,7 +1235,6 @@ namespace BWAPI
     {
       u->wasAlive = true;
       u->isAlive = false;
-      u->isDying = true;
     }
     lastEvadedUnits = evadeUnits;
     for each(UnitImpl* u in accessibleUnits)
@@ -1267,7 +1255,6 @@ namespace BWAPI
       if (isAlive(u))
       {
         u->isAlive = true;
-        u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
         u->updateInternalData();
@@ -1278,7 +1265,6 @@ namespace BWAPI
       if (isAlive(u,true))
       {
         u->isAlive = true;
-        u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
         u->updateInternalData();
@@ -1289,7 +1275,6 @@ namespace BWAPI
       if (isAlive(u))
       {
         u->isAlive = true;
-        u->isDying = false;
         aliveUnits.insert(u);
         dyingUnits.erase(u);
         u->updateInternalData();
@@ -1467,7 +1452,7 @@ namespace BWAPI
       }
       else
       {
-        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon)
           pylons.insert(u);
       }
     }
@@ -1488,7 +1473,7 @@ namespace BWAPI
       }
       else
       {
-        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+        if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon)
           pylons.erase(u);
       }
     }
@@ -1502,7 +1487,19 @@ namespace BWAPI
         for (int y = startY; y < endY; y++)
           unitsOnTileData[x][y].insert(i);
       if (i->lastType != i->_getType && i->lastType != UnitTypes::Unknown && i->_getType != UnitTypes::Unknown)
+      {
         events.push_back(Event::UnitMorph(i));
+        if (i->lastType == UnitTypes::Resource_Vespene_Geyser)
+        {
+          neutralUnits.erase(i);
+          geysers.erase(i);
+        }
+        if (i->_getType == UnitTypes::Resource_Vespene_Geyser)
+        {
+          neutralUnits.insert(i);
+          geysers.insert(i);
+        }
+      }
       if (i->lastPlayer != i->_getPlayer && i->lastPlayer != NULL && i->_getPlayer != NULL)
       {
         events.push_back(Event::UnitRenegade(i));

bwapi/BWAPI/Source/BWAPI/UnitImpl.cpp
@@ -27,7 +27,6 @@ namespace BWAPI
       , index(index)
       , userSelected(false)
       , isAlive(false)
-      , isDying(false)
       , wasAlive(false)
       , wasAccessible(false)
       , wasVisible(false)
@@ -2045,7 +2044,6 @@ namespace BWAPI
     index              = 0xFFFF;
     userSelected       = false;
     isAlive            = false;
-    isDying            = false;
     wasAlive           = false;
     wasAccessible      = false;
     wasVisible         = false;
@@ -2090,25 +2088,13 @@ namespace BWAPI
   // calls canAccess, setting error codes as needed
   bool UnitImpl::attemptAccess() const
   {
-    if (!BroodwarImpl.inUpdate)
-    {
-      BroodwarImpl.setLastError(Errors::None);
-      if (this->canAccess())
-        return true;
-      BroodwarImpl.setLastError(Errors::Unit_Not_Visible);
-      return false;
-    }
-    else
-    {
-      return canAccess();
-    }
+    BroodwarImpl.setLastError(Errors::None);
+    if (this->canAccess())
+      return true;
+    BroodwarImpl.setLastError(Errors::Unit_Not_Visible);
+    return false;
   }
 
-  //----------------------------------------------------------------------------------------------------------
-  std::string UnitImpl::getName() const
-  {
-    return this->getType().getName();
-  }
   //---------------------------------------------- UPDATE NEXT -----------------------------------------------
   UnitImpl* UnitImpl::getNext() const
   {

bwapi/BWAPI/Source/BWAPI/UnitImpl.h
@@ -218,7 +218,6 @@ namespace BWAPI
       bool attemptAccess() const;
       Player* _getPlayer;
       UnitType _getType;
-      std::string getName() const;
       Position _getPosition;
       int _getResources;
       int _getHitPoints;
@@ -250,7 +249,6 @@ namespace BWAPI
       UnitData data;
       UnitData* self;
 
-      bool dead;
       bool userSelected;
       bool staticInformation;
       bool nukeDetected;
@@ -260,10 +258,8 @@ namespace BWAPI
       bool startingAttack;
       UnitType lastType;
       Player* lastPlayer;
-      
       int id;
       bool isAlive;
-      bool isDying;
       bool wasAlive;
       bool wasAccessible;
       bool wasVisible;

bwapi/BWAPI/Source/DLLMain.cpp
@@ -197,7 +197,7 @@ void __declspec(naked) onIssueCommand()
 
   //decide if we should let the command go through
   if ( BWAPI::BroodwarImpl.isFlagEnabled(BWAPI::Flag::UserInput)
-       || !BWAPI::BroodwarImpl.isOnStartCalled()
+       || !BWAPI::BroodwarImpl.onStartCalled
        //If user input is disabled, only allow the following commands to go through:
        || commandID == 0x00 // Game Chat
        || commandID == 0x05 // Keep Alive

bwapi/BWAPIClient/Source/GameImpl.cpp
@@ -196,7 +196,7 @@ namespace BWAPI
         }
         else
         {
-          if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+          if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon)
             pylons.insert(u);
         }
       }
@@ -215,7 +215,7 @@ namespace BWAPI
         }
         else
         {
-          if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon && u->isCompleted())
+          if (u->getPlayer() == Broodwar->self() && u->getType() == UnitTypes::Protoss_Pylon)
             pylons.erase(u);
         }
       }
@@ -229,6 +229,20 @@ namespace BWAPI
         for each(Player* p in players)
           ((PlayerImpl*)p)->units.erase(u);
         ((PlayerImpl*)u->getPlayer())->units.insert(u);
+      }
+      else if (data->events[e].type==EventType::UnitMorph)
+      {
+        Unit* u=&unitVector[id];
+        if (u->getType()==UnitTypes::Resource_Vespene_Geyser)
+        {
+          geysers.insert(u);
+          neutralUnits.insert(u);
+        }
+        else if (u->getType().isRefinery())
+        {
+          geysers.erase(u);
+          neutralUnits.erase(u);
+        }
       }
     }
     if (getFrameCount()==1 && isFlagEnabled(Flag::CompleteMapInformation)==true)
@@ -558,6 +572,7 @@ namespace BWAPI
     /* Loop through all pylons for the current player */
     foreach (Unit* i, pylons)
     {
+      if (i->isCompleted()==false) continue;
       int px = i->getTilePosition().x();
       int py = i->getTilePosition().y();
       int bx = x - px + 7;