58dac6dcd8d72f59f32007026a0fd760c8c10fd7
Adam Heinermann <aheinerm@gmail.com>
2026-05-07 14:59:28 -0700

    Use a newer version of gtest to dodge a build error

--- cmake/LoadGoogleTest.cmake
+++ cmake/LoadGoogleTest.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 
-set(GTEST_VERSION 1.10.0)
+set(GTEST_VERSION 1.15.0)
 
 # Set Googletest CMake options
 set(BUILD_GMOCK ON CACHE BOOL "" FORCE)
@@ -11,7 +11,7 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 # Get Googletest source code
 FetchContent_Declare(googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG release-${GTEST_VERSION}
+  GIT_TAG v${GTEST_VERSION}
   GIT_SHALLOW ON
   GIT_PROGRESS ON
 )
